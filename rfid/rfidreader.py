5dc3e6aff51bb6358e93ca099b1720fdd02d6343
Vaibhav Jain <vaibhavjain30699@gmail.com>
2019-10-19 16:40:07 +0500

    added firebase functionality in RFID script

--- rfid/rfidreader.py
+++ rfid/rfidreader.py
@@ -1,10 +1,24 @@
 import RPi.GPIO as GPIO
 import sys
+import os
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "hackabit.json"
+
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'hackabit-bfd1f',
+})
+db = firestore.client()
+
 sys.path.append('/home/pi/MFRC522-python')
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
+
+doc_ref = db.collection(u'hardware').document(u'1')
 
 print("Hold a tag near the reader")
 
@@ -13,6 +27,7 @@ while(1):
 		id, text = reader.read()
 	 	print(id)
 		print(text)
+		doc_ref.set({u'id': id})
 
 	finally:
     		GPIO.cleanup()
